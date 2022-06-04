4830b83fd09779e1be0cfdb5a3f379be6093eb10
Leon van Zyl <leon.vanzyl@gmail.com>
2022-06-04 13:34:46 +0200

    Add CORS to mail API

--- api/contact.php
+++ api/contact.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Origin: *');
 // header('Content-Type: application/json');
 // header('Content-Type: *');
 // header('Content-Type: multipart/form-data');
