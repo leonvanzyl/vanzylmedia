a023060f70da52b4bac51fc1b675f8588811fae7
Leon van Zyl <leon.vanzyl@gmail.com>
2022-06-06 09:35:47 +0200

    Colors & Typography

--- script/main.js
+++ script/main.js
@@ -1,15 +1,3 @@
-const burgerMenu = document.querySelector(".burger-menu");
-const navItems = document.querySelector(".nav-items");
-// const submitBtn = document.querySelector("#submitbtn");
-
-burgerMenu.addEventListener("click", function () {
-  navItems.classList.toggle("burger-menu-active");
-});
-
-// submitBtn.addEventListener("click", function () {
-//   console.log("Test");
-// });
-
 // Form
 const emailInput = document.querySelector("#email");
 const nameInput = document.querySelector("#name");
@@ -22,10 +10,6 @@ const contactForm = document.querySelector("#contact-form");
 
 contactForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  console.log(emailInput.value);
-  console.log(nameInput.value);
-  console.log(phoneInput.value);
-  console.log(messageInput.value);
 
   const sendEmail = async () => {
     submitBtn.innerHTML = "Sending...";
@@ -57,3 +41,9 @@ contactForm.addEventListener("submit", async (e) => {
 
   await sendEmail();
 });
+
+// Set current copyright year
+
+const yearEl = document.querySelector("#year");
+const currentYear = new Date().getFullYear();
+yearEl.innerHTML = currentYear;
